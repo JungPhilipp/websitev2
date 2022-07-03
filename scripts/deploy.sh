3fae6f487b335593974e43a97fcf4bed7f936c84
Philipp Jung <philipp.jung@mailbox.org>
2022-07-03 14:20:35 +0200

    Only sync large files in local setup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-set -e
-rsync --archive --verbose --recursive --delete --human-readable  public/ pjungphilipp@philipp-jung.de:/web
+set -ex
+
+if [ -z ${INCLUDE_DATA+x} ]; then
+    rsync --archive --verbose --recursive --delete --human-readable --exclude='*.pdf' --exclude='*.pptx' public/ pjungphilipp@philipp-jung.de:/web
+else
+    rsync --archive --verbose --recursive --delete --human-readable                                      public/ pjungphilipp@philipp-jung.de:/web
+fi
+
