1d09bd179c79c67b7a51579bc52b26d82d56e601
Philipp Jung <philipp.jung@mailbox.org>
2022-07-03 13:43:52 +0200

    Use same username as local

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-DEPLOY_USERNAME_="${DEPLOY_USERNAME:-pjungphilipp}"
-rsync --archive --verbose --recursive --delete --human-readable  public/ ${DEPLOY_USERNAME_}@philipp-jung.de:/web
+set -e
+rsync --archive --verbose --recursive --delete --human-readable  public/ pjungphilipp@philipp-jung.de:/web
